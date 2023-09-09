3730868e37f21387faa869caf16d0b4b3993fceb
unknown <quangdung861@gmail.com>
2023-09-09 17:46:06 +0700

    Update client

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,2 +1,2 @@
 export const GRAPHQL_SERVER = "https://note-app-server-t227.onrender.com";
-export const GRAPHQL_SUBSCRIPTION_ENDPOINT = 'ws://note-app-server-t227.onrender.com/graphql';
+export const GRAPHQL_SUBSCRIPTION_ENDPOINT = 'wss://note-app-server-t227.onrender.com/graphql';
