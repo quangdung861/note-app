ac61918091377dcec707905f074fa54842d82adb
unknown <quangdung861@gmail.com>
2023-09-09 18:48:45 +0700

    update server

--- server/server.mjs
+++ server/server.mjs
@@ -1,27 +1,30 @@
-import express from 'express';
-import http from 'http';
-import { ApolloServer } from '@apollo/server';
-import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
-import bodyParser from 'body-parser';
-import { expressMiddleware } from '@apollo/server/express4';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import { makeExecutableSchema } from '@graphql-tools/schema';
-import { WebSocketServer } from 'ws';
-import { useServer } from 'graphql-ws/lib/use/ws';
+import express from "express";
+import http from "http";
+import { ApolloServer } from "@apollo/server";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import bodyParser from "body-parser";
+import { expressMiddleware } from "@apollo/server/express4";
+import cors from "cors";
+import mongoose from "mongoose";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import { WebSocketServer } from "ws";
+import { useServer } from "graphql-ws/lib/use/ws";
 
-import { resolvers } from './resolvers/index.js';
-import { typeDefs } from './schemas/index.js';
-import './firebaseConfig.js';
-import { getAuth } from 'firebase-admin/auth';
-
-import 'dotenv/config';
+import { resolvers } from "./resolvers/index.js";
+import { typeDefs } from "./schemas/index.js";
+import "./firebaseConfig.js";
+import "dotenv/config.js";
+import { getAuth } from "firebase-admin/auth";
 
 const app = express();
 const httpServer = http.createServer(app);
 
-// Connect to DB
-const URI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.wdmk4pf.mongodb.net/?retryWrites=true&w=majority`;
+// Query cho client muốn truy vấn dữ liệu
+// Mutation muốn update hay xóa dữ liệu gì đó
+// Subscription phía client muốn update theo dạng realtime
+
+// connect to DB
+const URI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.yffh0d6.mongodb.net/?retryWrites=true&w=majority`;
 const PORT = process.env.PORT || 4000;
 
 const schema = makeExecutableSchema({ typeDefs, resolvers });
@@ -32,7 +35,7 @@ const wsServer = new WebSocketServer({
   server: httpServer,
   // Pass a different path here if app.use
   // serves expressMiddleware at a different path
-  path: '/graphql',
+  path: "/graphql",
 });
 
 // Hand in the schema we just created and have the
@@ -40,11 +43,11 @@ const wsServer = new WebSocketServer({
 const serverCleanup = useServer({ schema }, wsServer);
 
 const server = new ApolloServer({
+  schema,
   // typeDefs,
   // resolvers,
-  schema,
   plugins: [
-    ApolloServerPluginDrainHttpServer({ httpServer }), // Proper shutdown for the WebSocket server.
+    ApolloServerPluginDrainHttpServer({ httpServer }),
     {
       async serverWillStart() {
         return {
@@ -62,9 +65,8 @@ await server.start();
 const authorizationJWT = async (req, res, next) => {
   console.log({ authorization: req.headers.authorization });
   const authorizationHeader = req.headers.authorization;
-
   if (authorizationHeader) {
-    const accessToken = authorizationHeader.split(' ')[1];
+    const accessToken = authorizationHeader.split(" ")[1];
 
     getAuth()
       .verifyIdToken(accessToken)
@@ -75,11 +77,11 @@ const authorizationJWT = async (req, res, next) => {
       })
       .catch((err) => {
         console.log({ err });
-        return res.status(403).json({ message: 'Forbidden', error: err });
+        return res.status(403).json({ message: "Forbidden", error: err });
       });
   } else {
     next();
-    // return res.status(401).json({ message: 'Unauthorized' });
+    // return res.status(401).json({ message: "Unauthorized" });
   }
 };
 
@@ -89,19 +91,20 @@ app.use(
   bodyParser.json(),
   expressMiddleware(server, {
     context: async ({ req, res }) => {
-      return { uid: res.locals.uid };
+      return {
+        uid: res.locals.uid,
+      };
     },
   })
 );
 
-mongoose.set('strictQuery', false);
 mongoose
   .connect(URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(async () => {
-    console.log('Connected to DB');
+    console.log("Connected to DB");
     await new Promise((resolve) => httpServer.listen({ port: PORT }, resolve));
-    console.log('🚀 Server ready at http://localhost:4000');
+    console.log("Server ready at port 4000");
   });
